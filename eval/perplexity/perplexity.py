2d80985d4c452823be2933347dc6df9060215202
Rohit Nagraj <rohitnagraj.99@gmail.com>
2024-10-23 15:53:06 -0700

    Created a script for perplexity

--- eval/perplexity/perplexity.py
+++ eval/perplexity/perplexity.py
@@ -1,15 +1,17 @@
-from typing import List
+from typing import List, Dict
 import pandas as pd
 import torch
-import numpy as np
 import math
-import json
-import os
+from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 class Perplexity:
     def __init__(self, model_name):
+        """
+
+        :param model_name: Huggingface model name
+        """
         self.batch_size = 32
 
         if torch.backends.mps.is_available():
@@ -29,9 +31,9 @@ class Perplexity:
         self.model.eval()
         self.model.to(self.device)
 
-    def _compute_token_probability(self, sentence):
-        inputs = self.tokenizer(sentence, return_tensors="pt").to(self.model.device)
+    def _compute_token_probability(self, text):
 
+        inputs = self.tokenizer(text, return_tensors="pt").to(self.model.device)
         # Get model logits (output probabilities before softmax)
         with torch.no_grad():
             outputs = self.model(**inputs)
@@ -50,16 +52,19 @@ class Perplexity:
         # Get tokens
         tokens = self.tokenizer.convert_ids_to_tokens(inputs["input_ids"][0].tolist())
 
-        # Exclude the first token <s> or [CLS]
-        return list(zip(tokens[1:], token_probs.squeeze().tolist()))
+        # Case when there's only one token prob
+        if type(token_probs.squeeze().tolist()) == float:
+            token_probs = list([token_probs.squeeze().tolist()])
+        else:
+            token_probs = token_probs.squeeze().tolist()
 
-    def _process_data(self, val_data):
-        # for all rows
+        return list(zip(tokens[1:], token_probs))
+
+    def _process_data(self, data):
         token_to_prob = {}
 
-        for index, row in val_data.iterrows():
-            input_text = row["Instruction"]
-            token_probs = self._compute_token_probability(input_text)
+        for index, d in enumerate(data):
+            token_probs = self._compute_token_probability(d)
             token_to_prob[index] = token_probs
 
         return token_to_prob
@@ -72,46 +77,16 @@ class Perplexity:
     def _calculate_perplexity(self, sentence_log_prob, num_tokens):
         return math.exp(-sentence_log_prob / num_tokens)
 
-    # Function to save checkpoint after each batch
-    def _save_checkpoint(self, perplexity_values, batch_idx, checkpoint_file):
-        checkpoint_data = {
-            "perplexity_values": perplexity_values,
-            "last_processed_batch": batch_idx,
-        }
-        with open(checkpoint_file, "w") as f:
-            json.dump(checkpoint_data, f)
+    # def _batch_process(self, data):
+    #     for i in range(0, len(data), self.batch_size):
+    #         yield data[i: i + self.batch_size]
 
-    # Function to load from the checkpoint
-    def _load_checkpoint(self, checkpoint_file):
-        if os.path.exists(checkpoint_file):
-            with open(checkpoint_file, "r") as f:
-                checkpoint_data = json.load(f)
-                return checkpoint_data["perplexity_values"], checkpoint_data[
-                    "last_processed_batch"
-                ]
-        else:
-            return [], -1  # No checkpoint found, start from scratch
+    def _calculate_perplexity_overall(self, data):
 
-    def _batch_process(self, data):
-        for i in range(0, len(data), self.batch_size):
-            yield data[i : i + self.batch_size]
-
-    def _process_data_in_batches(self, data):
-        results = {}
-        for batch_idx, batch_data in enumerate(self._batch_process(data)):
-            batch_results = self._process_data(batch_data)
-            results.update(batch_results)
-
-        return results
-
-    def _calculate_perplexity_overall(self, data, checkpoint_file=None):
-        # Load the checkpoint if it exists
-        # overall_perplexity, last_processed_batch = self._load_checkpoint(checkpoint_file)
         overall_perplexity = []
 
-        # Start from the next batch after the checkpoint
-        for batch_idx, batch_data in enumerate(self._batch_process(data)):
-
+        for text in tqdm(data):
+            batch_data = [text]
             batch_results = self._process_data(batch_data)
             for idx, tokens in batch_results.items():
                 token_probabilities = [prob for _, prob in tokens]
@@ -119,20 +94,13 @@ class Perplexity:
                 perplexity_value = self._calculate_perplexity(sentence_prob, len(token_probabilities))
                 overall_perplexity.append(perplexity_value)
 
-            # # Save checkpoint after each batch
-            # self._save_checkpoint(overall_perplexity, batch_idx, checkpoint_file)
-            # print(f"Batch {batch_idx + 1} processed, perplexity checkpoint saved.")
-
         return overall_perplexity
 
     def get_score(self, texts: List[str]) -> float:
-        """Calculates the perplexity score for a given HF model and a given dataset.
-
-        Args:
-            model (HF model): Model to calculate the perplexity score on.
-            data (List[str]): List of strings on which the perplexity is calculated and averaged over.
-        Returns:
-            float: The perplexity score
+        """
+        Calculate the perplexity score for a given HF model and a list of texts.
+        :param texts: List of strings.
+        :return: Float, the perplexity score.
         """
 
         overall_perplexity_val = self._calculate_perplexity_overall(texts)
@@ -141,8 +109,12 @@ class Perplexity:
 
 
 if __name__ == "__main__":
-    model = "meta-llama/Llama-3.2-1B-Instruct"
+    model = "unsloth/Llama-3.2-3B-Instruct"
+    # model = 'meta-llama/Llama-3.2-1B-Instruct'
     data = pd.read_csv('../../data/test.csv')
-    texts = data['output'].tolist()
+    def concat(x):
+        return x['instruction'] + "\n" + x['output']
+    texts = data.apply(lambda x: concat(x), axis=1)
     perplexity = Perplexity(model)
-    perplexity.get_score(texts)
+    score = perplexity.get_score(texts)
+    print(f"Perplexity Score: {score}")
